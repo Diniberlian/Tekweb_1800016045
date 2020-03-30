d15cc221fe26b6c06066b51281a0ad7bad313771
Diniberlian <62317647+Diniberlian@users.noreply.github.com>
2020-03-30 08:34:31 -0700

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tekweb1800016002'`, () => {
+  it(`should have as title 'tekweb1800016045'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tekweb1800016002');
+    expect(app.title).toEqual('tekweb1800016045');
   });
 
   it('should render title', () => {
